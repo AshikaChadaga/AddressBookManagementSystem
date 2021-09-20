e6d8d9a54c63a59c1fc9ac8d847c0ff44605897e
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-20 17:33:40 +0530

    REFACTORED: Tested method to write to and read from file

--- src/test/java/com/bridgelabz/addressbookmanagementsystem/AddressBookTest.java
+++ src/test/java/com/bridgelabz/addressbookmanagementsystem/AddressBookTest.java
@@ -1,16 +1,26 @@
 package com.bridgelabz.addressbookmanagementsystem;
 
+import java.util.List;
+
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AddressBookTest {
+	
+
+	static AddressBook addressBook = new AddressBook();
+	
+	@BeforeClass
+	public static void createAddressBookObject() {
+		addressBook.setAddressBookName("book1");
+	}
 	
 	@Test
 	public void givenDetails_ShouldAddToContactList() {
 		
 		ContactPerson person = new ContactPerson();
 		Address address = new Address();
-		AddressBook addressBook = new AddressBook();
 		
 		String firstName = "Ashika";
 		String lastName = "Satish";
@@ -51,6 +61,26 @@ public class AddressBookTest {
 		int listSize = addressBook.contactList.size();
 		Assert.assertEquals(2, listSize);
 		
+	}
+	
+	@Test
+	public void given2Contacts_WhenWrittenToFile_ShouldMatchEntries()
+	{
+		AddressBookFileIO addressFileIO = new AddressBookFileIO();
+		addressFileIO.writeToAddressBookFile("book1.txt", addressBook.contactList);
+		addressFileIO.printData("book1.txt");
+		long entries = addressFileIO.countEntries("book1.txt");
+		Assert.assertEquals(2, entries);
+		
+	}
+	
+	@Test
+	public void givenFile_WhenRead_ShouldReturnNumberOfEntries() {
+		
+		AddressBookFileIO addressFileIO = new AddressBookFileIO();
+		List<String> entries = addressFileIO.readDataFromFile("book1.txt");
+		long countEntries = entries.size();
+		Assert.assertEquals(2, countEntries);
 	}
 	
 	
